1062ba536d6c059bcb4f673e862d8819248ebc84
Nadilson <nadilson@protonmail.com>
2023-02-18 01:37:47 -0300

    Adicionado try/catch nos useEffect para evitar o erro Possible Unhandled Promise Rejection

--- App.tsx
+++ App.tsx
@@ -2,6 +2,8 @@ import {
   getCurrentPositionAsync,
   requestForegroundPermissionsAsync,
   LocationObject,
+  watchPositionAsync,
+  LocationAccuracy,
 } from "expo-location";
 import { useEffect, useState } from "react";
 import { View } from "react-native";
@@ -14,16 +16,42 @@ export default function App() {
   async function requestLocationPermissions() {
     const { granted } = await requestForegroundPermissionsAsync();
 
-    if (granted) {
-      const currentPosition = await getCurrentPositionAsync();
-      setLocation(currentPosition);
+    try {
+      if (granted) {
+        const currentPosition = await getCurrentPositionAsync();
+        setLocation(currentPosition);
 
-      console.log("LOCALIZAÇÃO ATUAL => ", currentPosition);
+        console.log("LOCALIZAÇÃO ATUAL => ", currentPosition);
+      }
+    } catch (error) {
+      console.log("getCurrentPositionAsync error: ", error);
     }
   }
 
   useEffect(() => {
-    requestLocationPermissions();
+    try {
+      requestLocationPermissions();
+    } catch (error) {
+      console.log("requestLocationPermissions error: ", error);
+    }
+  }, []);
+
+  useEffect(() => {
+    try {
+      watchPositionAsync(
+        {
+          accuracy: LocationAccuracy.Highest,
+          timeInterval: 1000,
+          distanceInterval: 1,
+        },
+        (response) => {
+          console.log("NOVA LOCALIZAÇÃO: ", response);
+          setLocation(response);
+        }
+      );
+    } catch (error) {
+      console.log("watchPositionAsync error: ", error);
+    }
   }, []);
 
   return (
